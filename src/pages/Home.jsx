7b522d42be28a3025f51fb8709d3adc8ec8b09db
Priyanshushekhar77 <way6shekhar@gmail.com>
2023-11-15 23:57:32 +0530

    home page done with footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-
 import {FaArrowRight} from "react-icons/fa"
 import {Link} from "react-router-dom"
-import HighlightText from '../components/core/Homepage/HighlightText'
+import HighlightText from '../components/core/HomePage/HighlightText'
 
-import CTAButton from "../components/core/Homepage/Button"
+import CTAButton from "../components/core/HomePage/Button"
 import Banner from "../assets/Images/banner.mp4"
-import CodeBlocks from "../components/core/Homepage/CodeBlocks"
+import CodeBlocks from "../components/core/HomePage/CodeBlocks"
+import TimelineSection from '../components/core/HomePage/TimelineSection'
+import LearningLanguageSection from '../components/core/HomePage/LearningLanguageSection'
+import InstructorSection from '../components/core/HomePage/InstructorSection'
+import Footer from '../components/common/Footer'
+import ExploreMore from '../components/core/HomePage/ExploreMore'
 
-export const Home = () => {
+const Home = () => {
   return (
     <div>
-          {/*Section1  */}
+      {/*Section1  */}
       <div className='relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center 
       text-white justify-between'>
 
@@ -28,7 +32,7 @@ export const Home = () => {
         </Link>
 
         <div className='text-center text-4xl font-semibold mt-7'>
-            Empower Your Future with Study-Mania
+            Empower Your Future with
             <HighlightText text={"Coding Skills"} />
         </div>
 
@@ -46,7 +50,7 @@ export const Home = () => {
             </CTAButton>
         </div>
 
-        <div className='mx-8 my-12 shadow-blue-200'>
+        <div className='mx-3 my-12 shadow-blue-200'>
             <video
             muted
             loop
@@ -124,18 +128,85 @@ export const Home = () => {
             />
         </div>
 
+            <ExploreMore />
       </div>
 
       {/*Section 2  */}
+      <div className='bg-pure-greys-5 text-richblack-700'>
+            <div className='homepage_bg h-[310px]'>
+
+                <div className='w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-5 mx-auto'>
+                    <div className='h-[150px]'></div>
+                    <div className='flex flex-row gap-7 text-white '>
+                        <CTAButton active={true} linkto={"/signup"}>
+                            <div className='flex items-center gap-3' >
+                                Explore Full Catalog
+                                <FaArrowRight />
+                            </div>
+                            
+                        </CTAButton>
+                        <CTAButton active={false} linkto={"/signup"}>
+                            <div>
+                                Learn more
+                            </div>
+                        </CTAButton>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+            <div className='mx-auto w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-7'>
+
+                <div className='flex flex-row gap-5 mb-10 mt-[95px]'>
+                    <div className='text-4xl font-semibold w-[45%]'>
+                        Get the Skills you need for a
+                        <HighlightText text={"Job that is in demand"} />
+                    </div>
+
+                    <div className='flex flex-col gap-10 w-[40%] items-start'>
+                    <div className='text-[16px]'>
+                    The modern StudyNotion is the dictates its own terms. Today, to be a competitive specialist requires more than professional skills.
+                    </div>
+                    <CTAButton active={true} linkto={"/signup"}>
+                        <div>
+                            Learn more
+                        </div>
+                    </CTAButton>
+                    </div>
+
+                </div>
+                
+                
+
+                <TimelineSection />
+
+                <LearningLanguageSection />
+
+            </div>
+
+            
+
+      </div>
 
 
       {/*Section 3 */}
+      <div className='w-11/12 mx-auto max-w-maxContent flex-col items-center justify-between gap-8 first-letter bg-richblack-900 text-white'>
+
+            <InstructorSection />
+
+            <h2 className='text-center text-4xl font-semobold mt-10'>review from Other Learners</h2>
+            {/* Review Slider here */}
+      </div>
 
 
       {/*Footer */}
-
+      <Footer />
 
     </div>
   )
 }
+
 export default Home
+
